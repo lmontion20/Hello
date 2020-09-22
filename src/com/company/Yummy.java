c124564b33fc5ba14362bd9c6dc14eb96e5c7b8b
laurenmontion <laurenmontion@billericak12.com>
2020-09-22 13:32:17 -0400

    methods

---
+++ src/com/company/Yummy.java
@@ -0,0 +1,33 @@
+package com.company;
+// New class to demonstrate methods
+public class Yummy {
+    // main method
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Goodbye");
+        chocolate();
+        iceCream();
+        System.out.println("done");
+    }
+
+    // chocolate method
+    public static void chocolate() {
+        System.out.println("Chocolate");
+    }
+    // French Fries method
+    public static void frenchFries() {
+        System.out.println("french fries with ketchup") ;
+        chocolate();
+        System.out.println("that was yummy") ;
+    }
+
+    // iceCream method calls frenchFries which then calls chocolate
+    public static void iceCream() {
+        System.out.println("ice") ;
+        frenchFries();
+        System.out.println("cream") ;
+    }
+
+}
+
+
